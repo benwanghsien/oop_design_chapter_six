5baf39dcd13737b3f6460105e6298060aeee48cd
Tgo Wang <ben2021345@hotmail.com>
2023-01-15 20:02:18 +0800

    add example 6-3

---
+++ bicycle/6-3.rb
@@ -0,0 +1,39 @@
+class Bicycle
+  attr_reader :size, :tape_color, :style, :front_cushion, :back_cushion
+
+  def initialize(args)
+    @size = args[:size]
+    @tape_color = args[:tape_color]
+    @style = args[:style]
+    @front_cushion = args[:front_cushion]
+    @back_cushion = args[:back_cushion]
+  end
+
+  def spares
+    {chain: "10-speed", tire_size: "23", tape_color: tape_color}
+  end
+end
+
+class MountainBike < Bicycle
+  attr_reader :front_cushion, :back_cushion
+
+  def initialize(args)
+    @front_cushion = args[:front_cushion]
+    @back_cushion = args[:back_cushion]
+    super(args)
+  end
+
+  def spares
+    super.merge(back_cushion: back_cushion)
+  end
+end
+
+mountain_bike_args = {
+  size: "S",
+  front_cushion: "Manitou",
+  back_cushion: "Fox",
+}
+
+mountain_bike = MountainBike.new(mountain_bike_args)
+puts mountain_bike.size # "S"
+puts mountain_bike.spares # {chain: "10-speed", tire_size: "2.1, tape_color: nil, back_cushion: "Fox"}
